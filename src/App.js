f2748f3f40817a6f81aa59b8a50b3611c1c32869
hailu ayalew <119626036+hailuayalew@users.noreply.github.com>
2023-01-02 16:48:19 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home';
+import Navbar from './pages/Navbar';
+import Add from './pages/user/Add';
+import Edit from './pages/user/Edit';
+import Users from './pages/user/Users';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+      <Routes>
+        <Route  path="/" exact element={<Home/>} />
+        <Route  path="/home" exact element={<Home/>} />
+        <Route  path="/student/:id" exact element={<Users/>} />
+        <Route  path="/add-student" exact element={<Add/>} />
+        <Route  path="/edit-student/:id" exact element={<Edit/>} />
+      </Routes>
     </div>
   );
 }
 
 export default App;
+///////
+///
+
